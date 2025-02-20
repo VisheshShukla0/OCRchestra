613dbe9152bacaccce696a791d6a55d5924e35af
Astitva Srivastava <asti1997.sri@gmail.com>
2025-02-21 04:21:24 +0530

    added tinyLLM code

---
+++ TinyLLM/llama.py
@@ -0,0 +1,25 @@
+import torch
+from transformers import pipeline
+
+
+def create_pipe():
+    model_id = "meta-llama/Llama-3.2-1B-Instruct"
+    pipe = pipeline(
+        "text-generation",
+        model=model_id,
+        torch_dtype=torch.bfloat16,
+        device_map="auto",
+    )
+    return pipe
+
+def ask( pipe, query):
+    messages = [
+    {"role": "system", "content": "You are a pirate chatbot who always responds in pirate speak!"},
+    {"role": "user", "content": f"{query}"},
+    ]
+    outputs = pipe(
+        messages,
+        max_new_tokens=256,
+    )
+    response = outputs[0]["generated_text"][-1]
+    return response
