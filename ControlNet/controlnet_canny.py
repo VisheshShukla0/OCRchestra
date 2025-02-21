8988303b057b8ae59de6b022c0bf4f3df0fab54e
Astitva Srivastava <asti1997.sri@gmail.com>
2025-02-21 06:40:23 +0530

    updated t2i pipeline

--- ControlNet/controlnet_canny.py
+++ ControlNet/controlnet_canny.py
@@ -6,7 +6,7 @@ import numpy as np
 import cv2
 
 
-def create_pipe():
+def create_pipe(cpu_offload=False):
     controlnet = ControlNetModel.from_pretrained(
         "diffusers/controlnet-canny-sdxl-1.0",
         torch_dtype=torch.float16
@@ -20,14 +20,14 @@ def create_pipe():
     )
     return pipe
 
-def generate( pipe, text_image, prompt):
-    canny_image = cv2.Canny(text_image, 100, 200)
+def generate( pipe, image, prompt):
+    canny_image = cv2.Canny(image, 100, 200)
     canny_image = canny_image[:, :, None]
     canny_image = np.concatenate([canny_image, canny_image, canny_image], axis=2)
     canny_image = Image.fromarray(canny_image)
     negative_prompt = 'low quality, bad quality, sketches'
     controlnet_conditioning_scale = 0.7
-    images = pipe(prompt, negative_prompt=negative_prompt, image=canny, controlnet_conditioning_scale=controlnet_conditioning_scale,
+    images = pipe(prompt, negative_prompt=negative_prompt, image=canny_image, controlnet_conditioning_scale=controlnet_conditioning_scale,
     ).images
     output = images[0]
     return output
